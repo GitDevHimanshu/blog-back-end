4c7cd1800d14fcc882a55e624dd25fe6a68557b1
himanshu <himanshu11199@gmail.com>
2025-03-29 21:35:10 +0530

    done

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const users = require('../model/signupSchema')
 const OTP = require("../model/OTPSchema")
 const nodemailer = require('../methods/nodeMailer')
@@ -22,6 +23,8 @@ module.exports.loginHandler = async(req, res)=>{
    let generatedOTP = await OTPGeneration(req.body.email)
    const status = await nodemailer.sendMail(req.body.email, generatedOTP);  
    if(status){
+    let token = jwt.sign({ userid: uid }, process.env.JWTKey , {expiresIn: '1h'})
+    res.cookie("jwt", token, { httpOnly: true, expires: new Date(Date.now() + 3600000) });
     res.status(200).json({message: "otp sent success"})
    }else{
     res.status(500).json({message: "internal server error"})
